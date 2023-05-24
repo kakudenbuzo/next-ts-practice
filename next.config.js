88eac3da038af7c1a9777dbfa0a229fb2e03fd22
kakudenbuzo <kakudenbuzou@gmail.com>
2023-05-24 20:57:50 +0200

    fix: Add prefix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: "export",
+  assetPrefix: isProd ? "/next-ts-practice/" : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
